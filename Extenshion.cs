b9ae101bd09f98235e784f088d39e2f438b8478c
Роман <ram23@mail.ru>
2022-05-10 18:09:12 +0300

    Добавьте файлы проекта.

---
+++ Extenshion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp6
+{
+    internal class Extenshion
+    {
+        public static void DirSize(DirectoryInfo di)
+        {
+            TimeSpan timeSpan = TimeSpan.FromMinutes(30);
+
+            FileInfo[] fl = di.GetFiles();
+
+            foreach (FileInfo fi in fl)
+            {
+                TimeSpan deltatime = DateTime.Now - File.GetLastWriteTime(fi.FullName);
+                
+                if (deltatime > timeSpan)
+                    fi.Delete();
+            }
+
+            DirectoryInfo[] dr = di.GetDirectories();
+
+            foreach (DirectoryInfo df in dr)
+            {
+                TimeSpan deltatime = DateTime.Now - Directory.GetLastWriteTime(df.FullName);
+
+                if (deltatime > timeSpan)
+                    df.Delete(true);
+
+                DirSize(df);
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo("C:/FinalTask1");
+
+                directoryInfo.Create();
+
+                if (directoryInfo.Exists)
+                {
+                    directoryInfo.CreateSubdirectory("SubFolder");
+                    directoryInfo.CreateSubdirectory("SubFolder2");
+                }
+                else
+                    Console.WriteLine("папка по заданному адресу не существует");
+
+
+                FileInfo fileInfo = new FileInfo("C:/FinalTask1/SubFolder/newFile.txt");
+
+                fileInfo.Create();
+
+                if (fileInfo.Exists)
+                {
+                    Console.WriteLine($"Создан файл с именем {fileInfo.FullName}");
+                }
+                else
+                    Console.WriteLine($"файл {fileInfo.FullName} не существует") ;
+
+                
+                fileInfo = new FileInfo("C:/FinalTask1/SubFolder/newFile2.txt");
+
+                    fileInfo.Create();
+
+                if (fileInfo.Exists)
+                {
+                    Console.WriteLine($"Создан файл с именем {fileInfo.FullName}");
+                }
+                else
+                    Console.WriteLine($"файл {fileInfo.FullName} не существует");
+
+                Extenshion.DirSize(directoryInfo);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
+
+
+
+
